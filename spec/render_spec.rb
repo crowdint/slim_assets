26c47ecd79b10eb942a258a2874b7d1fad23e622
David Padilla <david@crowdint.com>
2012-02-17 16:35:09 -0600

    Failing test for weird form issue

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
+class Post < ActiveRecord::Base
+end
+
 describe SlimAssets do
   it "should have the proper format" do
     RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s.should == "(function() {\n  this.JST || (this.JST = {});\n  this.JST[\"link_to\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1><div class=\"text\">Render works</div></h1>');}return __p.join('');};\n}).call(this);\n"
+  end
+
+  it "should use rails helpers" do
+    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should == "(function() {\n  this.JST || (this.JST = {});\n  this.JST[\"helpers\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1>Form</h1><form accept-charset=\"UTF-8\" action=\"/\" class=\"new_post\" id=\"new_post\" method=\"post\"><div style=\"margin:0;padding:0;display:inline\"><input name=\"utf8\" type=\"hidden\" value=\"&#x2713;\" /></div><input id=\"post_title\" name=\"post[title]\" size=\"30\" type=\"text\" /></form>');}return __p.join('');};\n}).call(this);\n"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,15 @@ require 'rails_app/config/environment'
 require 'slim_assets'
 require 'ejs'
 require 'rspec/rails'
+require 'active_record'
+
+ActiveRecord::Base.
+  establish_connection(adapter: "sqlite3", database: ":memory:")
+
+ActiveRecord::Schema.define(:version => 0) do
+  create_table "posts", :force => true do |t|
+    t.string "title"
+  end
+end
+
+
