e447bd4bd14ed65cacbd6b2d960f77bcf71527c9
Matt Vague <mattvague@gmail.com>
2013-05-17 13:23:59 -0700

    Application helpers are now availible to slim templates

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -11,4 +11,8 @@ describe SlimAssets do
   it "should use rails helpers" do
     RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should == "(function() {\n  this.JST || (this.JST = {});\n  this.JST[\"helpers\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1>Form</h1><form accept-charset=\"UTF-8\" action=\"/\" class=\"new_post\" id=\"new_post\" method=\"post\"><div style=\"margin:0;padding:0;display:inline\"><input name=\"utf8\" type=\"hidden\" value=\"&#x2713;\" /></div><input id=\"post_title\" name=\"post[title]\" size=\"30\" type=\"text\" /></form>');}return __p.join('');};\n}).call(this);\n"
   end
+
+  it "should use application helpers" do
+    RailsApp::Application.assets['app_helpers.jst.ejs.slim'].to_s.should == "(function() {\n  this.JST || (this.JST = {});\n  this.JST[\"app_helpers\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1>App helpers work</h1>');}return __p.join('');};\n}).call(this);\n"
+  end
 end
